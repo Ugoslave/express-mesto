b5b3390273fc679f1669a72179eb932e431a2ef1
Alexey Peresypkin <ugo200@yandex.ru>
2021-06-12 17:14:02 +0500

    Composed: Creating new user with bugs

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/users', require('./routes/users'));
+
+app.listen(PORT);
